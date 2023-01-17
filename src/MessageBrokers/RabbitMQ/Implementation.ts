003ce5c9d991a9e69123a66b8622b221add3d293
FueledByRage <erkmgwr@gmail.com>
2023-01-17 12:23:11 -0300

    Adding the connection to RabbitMQ

--- src/MessageBrokers/RabbitMQ/Implementation.ts
+++ src/MessageBrokers/RabbitMQ/Implementation.ts
@@ -1,14 +1,29 @@
-import { Channel } from "amqplib";
 import { DTOSendMessageStream } from "../../services/DTOs/DTOsendMessageStream";
 import { IMessageStream } from "../../services/messageStream/IMessageStream";
+import { errorFactory } from "../../utils/errorFactory";
+import client, { Connection, Channel } from 'amqplib';
+import * as dotenv from 'dotenv'; 
+dotenv.config();
 
-export const RabbitMqImplementation = ( channel : Channel ) : IMessageStream =>{
+export const RabbitMqImplementation = () : IMessageStream =>{
+
+    const getConnection = async ( queue : string ) =>{
+        const { MESSAGE_BROKER_HOST } = process.env;
+        if(!MESSAGE_BROKER_HOST) return null;
+        const connection : Connection = await client.connect(MESSAGE_BROKER_HOST);
+        const channel : Channel = await connection.createChannel();
+        return channel;
+    }
 
     return{
         sendMessage(data : DTOSendMessageStream ) : Promise<void> {
-            return new Promise((resolve, reject) =>{
+            return new Promise( async (resolve, reject) =>{
                 try {
+                    const channel : Channel | null = await getConnection(data.queue);
+                    if(!channel) throw errorFactory('Connection error to the message broker');
+                    await channel.assertQueue(data.queue);
                     channel.sendToQueue( data.queue, Buffer.from(data.message));
+                    await channel.close()
                     resolve();
                 } catch (error) {
                     reject(error);
--- src/MessageBrokers/RabbitMQ/connection/index.ts
+++ src/MessageBrokers/RabbitMQ/connection/index.ts
@@ -2,11 +2,10 @@ import client, { Connection, Channel } from 'amqplib';
 import * as dotenv from 'dotenv'; 
 dotenv.config();
 
-const getChannel = async ( queue : string ) =>{
+export const getChannel = async ( queue : string ) =>{
     const { MESSAGE_BROKER_HOST } = process.env;
     if(!MESSAGE_BROKER_HOST) return null;
     const connection : Connection = await client.connect(MESSAGE_BROKER_HOST);
     const channel : Channel = await connection.createChannel();
-    await channel.assertQueue( queue );
-    return channel;
+    return await channel.assertQueue( queue );
 }
