c68be6b33c322c5c52fcebe40601c10d6dbf54d8
FueledByRage <erkmgwr@gmail.com>
2023-01-16 21:58:26 -0300

    Adding an amqp implementation to the message broker abstraction

--- src/MessageBrokers/RabbitMQ/Implementation.ts
+++ src/MessageBrokers/RabbitMQ/Implementation.ts
@@ -1,12 +1,18 @@
+import { Channel } from "amqplib";
 import { DTOSendMessageStream } from "../../services/DTOs/DTOsendMessageStream";
 import { IMessageStream } from "../../services/messageStream/IMessageStream";
 
-export const RabbitMqImplementation = (  ) : IMessageStream =>{
+export const RabbitMqImplementation = ( channel : Channel ) : IMessageStream =>{
 
     return{
         sendMessage(data : DTOSendMessageStream ) : Promise<void> {
             return new Promise((resolve, reject) =>{
-                
+                try {
+                    channel.sendToQueue( data.queue, Buffer.from(data.message));
+                    resolve();
+                } catch (error) {
+                    reject(error);
+                }
             });
         },
     }
